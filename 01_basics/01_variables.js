ccdb12db104d08c9e071beaa9b3739a745595a12
Himanshu4368 <114290034+Himanshu4368@users.noreply.github.com>
2024-09-12 13:42:49 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,17 @@
+const accountId = 144553
+let accountEmail = "himanshucom72@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountState;
+// accountId = 2 //not allowed 
+
+accountEmail = "himanshuv.ip.22@nitj.ac.in"
+accountPassword = "1255785"
+accountCity = "Bengalore"
+console.log(accountId);
+/*
+prefer not to use var 
+because of issue in block scope and functional scope 
+*/
+
+console.table([accountEmail,accountPassword,accountCity,accountState])
