9e49b77130cb3d0418cb2b04db99eec29b130e11
Himanshu4368 <114290034+Himanshu4368@users.noreply.github.com>
2024-09-12 13:42:29 +0000

    dattypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,20 @@
+"use strict" //treat all JS code as newer version.
+// alert(3+3) //we are using nodejs,not browser
+
+let name = "Himanshu"
+let age = 18
+let isLoggedIn = false
+
+//number => 2to power 53
+//bigint is used for big numbers only
+//string => always use it in douuble quotes ("")
+//boolean => true/false
+//null => standalone value
+//undefined => 
+//symbol => unique
+
+//object
+
+console.log(typeof"hitesh");
+console.log(typeof null);// output is object
+console.log(typeof undefined) //output is undefined
